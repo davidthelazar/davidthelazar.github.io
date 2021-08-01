927847291e86ee05b90c5070f536a7ae62ca9153
David Lazar <triplequark@gmail.com>
2021-08-01 14:49:13 -0800

    Testing geocities site

--- test/blaseballFeed.js
+++ test/blaseballFeed.js
@@ -1,4 +1,4 @@
-eventSource = new EventSource(`https://api.sibr.dev/replay/v1/replay?from=2020-08-30T01:00:08.17Z`);
+eventSource = new EventSource(`https://api.sibr.dev/replay/v1/replay?from=2020-08-27T01:00:08.17Z`);
 const gameIdx = 0;
 let gameId = undefined;
 eventSource.onmessage = function(event)
@@ -7,16 +7,23 @@ eventSource.onmessage = function(event)
     {
         update = JSON.parse(event.data);
         games = update.value.games.schedule;
+		
+        // if(gameId === undefined)
+//         {
+//
+//         }
+	    $("#updates").html("<ul></ul>");
+		for (let idx = 0; idx < games.length; idx++) 
+		{
+            gameId = games[idx].id;
+			thisGame = games.filter(function(x) { return x.id === gameId; })[0];
 
-        if(gameId === undefined)
-        {
-            gameId = games[gameIdx].id;
-        }
-		thisGame = games.filter(function(x) { return x.id === gameId; })[0];
-
-        console.log(thisGame);
-	    $("#updates").text(thisGame.lastUpdate);
-
+	        console.log(thisGame);
+		    // $("#updates").text(thisGame.lastUpdate);
+			$("#updates").append("<li>"+thisGame.lastUpdate+"</li>");
+			
+		}
+		
     }
     catch(err)
     {
