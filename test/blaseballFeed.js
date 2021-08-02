919a8fe77b775071a059bb68ff3ca3b2ac1679ed
David Lazar <triplequark@gmail.com>
2021-08-01 16:10:00 -0800

    Adds datepicker to gleocities and now shows all games

--- test/blaseballFeed.js
+++ test/blaseballFeed.js
@@ -1,35 +1,46 @@
-eventSource = new EventSource(`https://api.sibr.dev/replay/v1/replay?from=2020-08-27T01:00:08.17Z`);
+// eventSource = new EventSource(`https://api.sibr.dev/replay/v1/replay?from=2020-08-27T01:00:08.17Z`);
 const gameIdx = 0;
 let gameId = undefined;
-eventSource.onmessage = function(event)
-{
-    try
-    {
-        update = JSON.parse(event.data);
-        games = update.value.games.schedule;
-		
-        // if(gameId === undefined)
-//         {
-//
-//         }
-	    $("#updates").html("<ul></ul>");
-		for (let idx = 0; idx < games.length; idx++) 
-		{
-            gameId = games[idx].id;
-			thisGame = games.filter(function(x) { return x.id === gameId; })[0];
+this.timeGrabber = document.getElementById('startTime');
+// const timeGrabber = $('#startTime')
+let self = this;
+this.eventSource = new EventSource(`https://api.sibr.dev/replay/v1/replay?from=2020-08-27T01:00:08.17Z`);
+this.timeGrabber.addEventListener('change', (event) => {
+	self.eventSource.close();
+	self.eventSource = new EventSource(`https://api.sibr.dev/replay/v1/replay?from=${event.target.value}`);
+	self.eventSource.onmessage = function(event)
+	{
+	    try
+	    {
+	        update = JSON.parse(event.data);
+	        games = update.value.games.schedule;
+	
+	        // if(gameId === undefined)
+	//         {
+	//
+	//         }
+		    $("#updates").html("<ul></ul>");
+			for (let idx = 0; idx < games.length; idx++) 
+			{
+	            gameId = games[idx].id;
+				thisGame = games.filter(function(x) { return x.id === gameId; })[0];
 
-	        console.log(thisGame);
-		    // $("#updates").text(thisGame.lastUpdate);
-			$("#updates").append("<li>"+thisGame.lastUpdate+"</li>");
-			
-		}
+		        console.log(thisGame);
+			    // $("#updates").text(thisGame.lastUpdate);
+				$("#updates").append("<li>"+thisGame.lastUpdate+"</li>");
 		
-    }
-    catch(err)
-    {
-        console.log(err.message);
-    }
-}
-    
-    
+			}
+	
+	    }
+	    catch(err)
+	    {
+	        console.log(err.message);
+	    }
+	}
+
+	
+	
+});
+
+
     
