fc5799c5372e305101886b1f21846d4b0ee4c656
David Lazar <triplequark@gmail.com>
2021-08-07 00:00:32 -0800

    Removing failed guestbook

--- test/guestbook/postform.php
+++
@@ -1,9 +0,0 @@
-<div class="normalText">
-<form action="addbook.php" method="post">
-Message<br><br>
-<textarea name="message" cols="50" rows="7" class="textbox"></textarea><br><br>
-Name<br><br>
-<input type="text" name="name" size="48" class="textbox"><br><br>
-<input type="submit" value=" Submit " class="textbox">
-</form>
-</div>
